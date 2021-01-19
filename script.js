9f2723445426ab138a826c0ee32e3e5f075de276
Josefine Honoré Ungermann <josefinehonoreungermann@Josefines-MacBook-Pro.local>
2021-01-19 20:45:51 +0100

    slettet kommentarer

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
-
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
 function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
-
+    //med toggle kan jeg tilføje noget der ikke er der og fjerne noget der er der, hidden
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
-
     if (erSkjult == true) {
         document.querySelector("#menuknap").textContent = "☰";
+        //hvis der er rigtigt så er der en synlig burgermenu
     } else {
         document.querySelector("#menuknap").textContent = "X";
     }
